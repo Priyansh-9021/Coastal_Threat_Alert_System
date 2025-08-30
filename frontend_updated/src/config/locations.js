75f4d4797dfc2565cc80ac0bb199c0c6c6c15969
Priyansh-9021 <priyanshsoneji9021@gmail.com>
2025-08-30 20:57:29 +0530

    Deleted folder frontend

--- frontend_updated/src/config/locations.js
+++
@@ -1,8 +0,0 @@
-export const coastalLocations = [
-  { name: "Porbandar", lat: 21.6417, lng: 69.6293 },
-  { name: "Okha", lat: 22.4676, lng: 69.0702 },
-  { name: "Dwarka", lat: 22.2442, lng: 68.9685 },
-  { name: "Jamnagar", lat: 22.4707, lng: 70.0577 },
-  { name: "Veraval", lat: 20.9075, lng: 70.3679 },
-  { name: "Bhavnagar", lat: 21.7645, lng: 72.1519 }
-];
